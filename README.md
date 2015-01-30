89d3802cec6fed3d51ea1e0453eba8cae6c5e9c9
N Oliver <github@pixnbits.org>
2015-01-30 12:06:02 -0700

    actually start/get driver and connect to karma

---
+++ README.md
@@ -0,0 +1,61 @@
+# karma-selenium-webdriver-launcher
+
+> Use a custom [Selenium Webdriver](http://www.seleniumhq.org/) instance. Useful for iron fist control, ex: extension testing.
+
+
+## Installation
+
+The easiest way is to keep `karma-selenium-webdriver-launcher` as a devDependency in your `package.json`.
+```json
+{
+  "devDependencies": {
+    "karma": "~0.10",
+    "karma-selenium-webdriver-launcher": "~0.1"
+  }
+}
+```
+
+You can also add it by this command:
+```bash
+npm install karma-selenium-webdriver-launcher --save-dev
+```
+
+
+## Configuration
+
+```js
+// karma.conf.js
+var By = require('selenium-webdriver').By,
+    until = require('selenium-webdriver').until,
+    firefox = require('selenium-webdriver/firefox');
+
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine'],
+
+    // define browsers
+    customLaunchers: {
+      swd_firefox: {
+        base: 'SeleniumWebdriver',
+        browserName: 'Firefox',
+        getDriver: function(){
+          // example from https://www.npmjs.com/package/selenium-webdriver#usage
+          var driver = new firefox.Driver();
+          return driver;
+        }
+      },
+    },
+
+    browsers: ['swd_firefox']
+  });
+};
+```
+
+### Browser options
+- `browserName` name of the browser (ideally we'll use `Capabilities` in the future)
+- `getDriver` function that will return a webdriver instance.
+
+
+----
+
+For more information on Karma see the [homepage](http://karma-runner.github.io).
--- index.js
+++ index.js
@@ -1,13 +1,78 @@
 var q = require('q');
-var webdriver = require('selenium-webdriver');
+//var webdriver = require('selenium-webdriver');
 
 
 
-var SeleniumWebdriverBrowser = function(id, emitter, args, logger,
-                                   /* config */ config) {
+function SeleniumWebdriverBrowser(id, emitter, args, logger, config) {
+  var self = this;
+  var captured = false;
+  var log = logger.create('launcher.selenium-webdriver');
+  var browserName = args.browserName;
 
+  this.id = id;
+  self.setName(browserName);
+
+  log.info('SeleniumWebdriverBrowser (kid:'+id+') created');
+
+  this.start = function(url){
+    log.info('starting '+self.name);
+    var driver = args.getDriver();
+    self.driver_ = driver;
+    
+    self.getSession_(function(session){
+      // TODO: caps_ might be a defer as well
+      self.setName(session.caps_.caps_);
+    });
+
+    log.info('sending driver to url '+url);
+    driver.get(url);
+  };
+
+  this.kill = function(){
+    self.getSession_(function(session){
+      log.info('requested to kill, session id is '+(session && session.id_));
+      if(session.id_){
+        this.driver_ && this.driver_.quit();
+      }
+    });
+  }
+
+}
+
+
+SeleniumWebdriverBrowser.prototype.setName = function(arg) {
+  // arg is either string and assumed to be `browserName` parameter
+  // or is object, being the session capabilities
+  var browserName;
+  if('string' === typeof arg){
+    browserName = arg;
+  }else if(arg && arg.version){
+    browserName = (arg.browserName || arg.device) + (arg.version ? ' ' + arg.version : '') + ' (' + arg.platform +  ')';
+  }
+
+  if(browserName){
+    this.name = browserName + ' via Selenium Webdriver';
+    console.log('changed name to '+this.name);
+  }
 };
 
+SeleniumWebdriverBrowser.prototype.getSession_ = function(cb) {
+  var driver = this.driver_;
+
+  if(!driver){
+    return cb(null);
+  }
+
+  if(driver.session_ && driver.session_.then){
+    driver.session_.then(cb);
+  }else{
+    cb(driver.session_);
+  }
+};
+
+SeleniumWebdriverBrowser.prototype.toString = function() {
+  return this.name || 'Unnamed SeleniumWebdriverBrowser';
+};
 
 // PUBLISH DI MODULE
 module.exports = {
